1796a6b09ae6518ababa19eca1f1d931d597a510
lmaoclost <renansmoliveira@gmail.com>
2022-10-14 22:50:05 -0300

    after class

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
+import { SignIn } from "./pages/SignIn";
+
 import "./styles/global.css";
 
 export function App() {
-  return (
-    <>
-      <h1 className="font-bold text-5xl text-violet-500">Hello World!</h1>
-
-      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
-        Enviar
-      </button>
-    </>
-  );
+  return <SignIn />;
 }
